d87a8444bd201df24c45f9110695d6ea329bf220
karna <kisanthapamagar@gmail.com>
2018-02-08 12:08:44 -0600

    First Commit of 3 Different Project

--- WebAppnTier/WebAppnTier/Controllers/EmployeeController.cs
+++ WebAppnTier/WebAppnTier/Controllers/EmployeeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BusinessLayer;
+// Using MVC in 3 tier Acrhitecture
 namespace WebAppnTier.Controllers
 {
     public class EmployeeController : Controller
